eeb693922f752b816ca91e6434aa173ecd080cce
cr7st74n <cris-082011@hotmail.com>
2023-03-20 20:49:55 -0400

    changing some file names for better understanding

--- app/app.js
+++ app/app.js
@@ -15,11 +15,24 @@ const app =  Vue.createApp({
                 console.log('good');
                 alert("No more names !")
              }else{
-                this.message = this.message +" " +this.name +"!"
+                // this.message = this.message +" " +this.name +"!"
                 this.count++;
                 console.log(this.count);
              }
 
+        },
+        ClearExclamation(){
+            // this.message = "hi student";
+            this.count = 0
+        }
+    },
+    computed: {
+        messageWithExcPoint(){
+            let messageWpoints = this.message;
+            for (let x=0 ; x < this.count; x++){
+                messageWpoints = messageWpoints +" " +this.name +"!"
+            }
+            return messageWpoints
         }
     }
 })
